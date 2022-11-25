1e904fc7d435cc784dee9348d8c1ddd5d37c96a9
peeravali0110 <peeravali0110@gmail.com>
2022-11-25 23:48:10 +0530

    okay

---
+++ javascript2/two.js
@@ -0,0 +1,14 @@
+var a=20;
+        var b=30
+        var sum=a+b;
+        var d="aryan"
+        var e=true
+        var f;
+        console.log(a+b);
+        console.log(sum);
+        console.log(e)
+        console.log(typeof a);
+        console.log(typeof b);
+        console.log(typeof d);
+        console.log(typeof e);
+        
