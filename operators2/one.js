fc5343ad368d242c978e8c2f184c25752f6e39a4
peeravali0110 <peeravali0110@gmail.com>
2022-11-26 22:27:41 +0530

    okkk

---
+++ operators2/one.js
@@ -0,0 +1,13 @@
+let sal = 75000
+
+if (sal > 95000) {
+    console.log("SYED PEERA VALI")
+      console.log("GM")
+     console.log("GA")
+     console.log("GN")
+}
+else{
+
+  console.log("aryan")
+
+}
---
+++ operators2/two.js
@@ -0,0 +1,8 @@
+let num=999
+if(num % 2 == 0){
+    console.log("odd number")
+
+}
+else{
+    console.log("even number")
+}
