52755ec265d113d9e8b5348185cff65709e9a00e
peeravali0110 <peeravali0110@gmail.com>
2022-11-28 22:14:32 +0530

    okay

---
+++ ittarativestatements/fibo.js
@@ -0,0 +1,24 @@
+/*let a=40
+let b=15
+while( a<1000){
+
+    console.log(a+ "")
+    c=a+b
+    a=b
+    b=c
+} */
+
+
+
+
+let a=0;
+let b=1;
+console.log("fibonacci sries")
+for(let i=1;i<9;i++){
+
+
+console.log(a)
+    c=a+b;
+    a=b;
+    b=c;
+}
