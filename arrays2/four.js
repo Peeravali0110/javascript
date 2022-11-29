28b70df46302fa15d2f482fb9d961917dc73bdec
peeravali0110 <peeravali0110@gmail.com>
2022-11-29 19:47:50 +0530

    okay

---
+++ arrays2/four.js
@@ -0,0 +1,22 @@
+// using do while
+
+
+let size=[38,39,42,44,46,48]
+
+size[1]=40
+
+
+let i=0;
+do {
+    console.log(size[i])
+    i=i+1
+}
+while(i<=size.length-1){
+}
+
+
+
+console.log(size.length)
+
+
+console.log(size[size.length-1])
