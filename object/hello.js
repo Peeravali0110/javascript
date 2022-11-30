51eb25286ef24a91dc1f1c2874c990afabb527fa
peeravali0110 <peeravali0110@gmail.com>
2022-11-30 20:55:09 +0530

    okkkk

---
+++ object/hello.js
@@ -0,0 +1,49 @@
+let empdetails={
+
+    name:"syed peera vali",
+    id:1293891,
+    age:23,
+    salary:650000,
+    location:["giddalur","hyderabad","mumbai","newdelhi"]
+}
+console.log(empdetails.name)
+console.log(empdetails.id)
+console.log(empdetails.age)
+console.log(empdetails.salary)
+
+console.log(empdetails.location[0])
+console.log(empdetails.location[1])
+console.log(empdetails.location[2])
+console.log(empdetails.location[3])
+
+
+console.log(empdetails.location.length)
+console.log(empdetails.location[empdetails.location.length-1])
+
+
+
+
+
+
+
+
+
+
+/*
+//using for loop
+
+for( let i=0;i<=empdetails.location.length-1;i=i+1){
+
+    console.log(empdetails.location[i])
+
+}
+
+
+//using whille loop
+ 
+let i=0;
+while(i<=empdetails.location.length-1){
+
+    console.log(empdetails.location[i])
+    i=i+1
+}*/
