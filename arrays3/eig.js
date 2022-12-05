9cbb2356d60ac1de3612d882164ba7715e61006e
peeravali0110 <peeravali0110@gmail.com>
2022-12-05 10:58:06 +0530

    okay

---
+++ arrays3/eig.js
@@ -0,0 +1,26 @@
+let emp=[{"id":1,"first_name":"Marguerite","last_name":"O'Howbane","email":"mohowbane0@geocities.jp","gender":"Female"},
+{"id":2,"first_name":"Orrin","last_name":"Belfrage","email":"obelfrage1@wikipedia.org","gender":"Male"},
+{"id":3,"first_name":"Roderic","last_name":"Malthus","email":"rmalthus2@hao123.com","gender":"Male"},
+{"id":4,"first_name":"Saunderson","last_name":"Samworth","email":"ssamworth3@biglobe.ne.jp","gender":"Male"},
+{"id":5,"first_name":"Norbie","last_name":"Batalini","email":"nbatalini4@yale.edu","gender":"Male"},
+{"id":6,"first_name":"Desi","last_name":"McNess","email":"dmcness5@over-blog.com","gender":"Male"},
+{"id":7,"first_name":"Ossie","last_name":"MacGinley","email":"omacginley6@tuttocitta.it","gender":"Male"},
+{"id":8,"first_name":"Arvie","last_name":"Roderham","email":"aroderham7@time.com","gender":"Male"},
+{"id":9,"first_name":"Corina","last_name":"Latham","email":"clatham8@state.gov","gender":"Female"},
+{"id":10,"first_name":"Colleen","last_name":"Shakelade","email":"cshakelade9@myspace.com","gender":"Female"},
+{"id":11,"first_name":"Ingar","last_name":"Manueau","email":"imanueaua@ibm.com","gender":"Male"},
+{"id":12,"first_name":"Lucy","last_name":"Leynham","email":"lleynhamb@tiny.cc","gender":"Genderqueer"},
+{"id":13,"first_name":"Quillan","last_name":"Lindley","email":"qlindleyc@themeforest.net","gender":"Agender"},
+{"id":14,"first_name":"Brooke","last_name":"Barbary","email":"bbarbaryd@themeforest.net","gender":"Male"},
+{"id":15,"first_name":"Morly","last_name":"McLice","email":"mmclicee@cpanel.net","gender":"Male"},
+{"id":16,"first_name":"Park","last_name":"Doiley","email":"pdoileyf@dell.com","gender":"Male"},
+{"id":17,"first_name":"Estevan","last_name":"Mityushkin","email":"emityushking@webmd.com","gender":"Bigender"},
+{"id":18,"first_name":"Piotr","last_name":"Antognazzi","email":"pantognazzih@apache.org","gender":"Male"},
+{"id":19,"first_name":"Terrye","last_name":"Dossett","email":"tdossetti@disqus.com","gender":"Female"},
+{"id":20,"first_name":"Carina","last_name":"Labet","email":"clabetj@behance.net","gender":"Female"}]
+
+for(  x of emp){
+
+    console.log(x)
+    console.log(emp[2].email)
+}
